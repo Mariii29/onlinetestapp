6ae3cf883d3b6e297eada6cd2176ca4b25209f25
Mariii29 <marisia.masao@vodacom.co.tz>
2023-10-06 19:49:59 +0300

    Added modules and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { QuizModule } from './quiz/quiz.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    QuizModule,
     AppRoutingModule
   ],
   providers: [],
